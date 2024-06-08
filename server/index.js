e09a895c2332db52ef5f961f725482401e1db61a
novadityap <novaaditya23@gmail.com>
2024-06-09 01:40:44 +0700

    feat: Add register endpoint for user registration

--- server/index.js
+++ server/index.js
@@ -2,12 +2,14 @@ import express from 'express';
 import logger from './config/logger.js';
 import dbConnect from './config/dbConnect.js';
 import errorMiddleware from './middlewares/errorMiddleware.js';
+import authRoute from './routes/authRoute.js';
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
 dbConnect();
 
+app.use('/api/auth', authRoute);
 app.use(errorMiddleware);
 
 app.listen(3000, () => {
