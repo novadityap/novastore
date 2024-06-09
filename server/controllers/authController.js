6fa73134712a6cb5f636167bd053612e0ec25de6
novadityap <novaaditya23@gmail.com>
2024-06-09 22:38:04 +0700

    fix: Auth controller for user registration

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,19 +1,28 @@
 import User from '../models/userModel.js';
 import validate from '../validations/validate.js';
-import { registerSchema } from '../validations/authValidation.js';
+import {registerSchema} from '../validations/authValidation.js';
 import sendMail from '../config/sendMail.js';
 import ejs from 'ejs';
+import jwt from 'jsonwebtoken';
 
 const register = async (req, res, next) => {
   try {
-    let value = validate(registerSchema, req.body);
-    const user = await User.create(value);
-    const html = await ejs.renderFile('./views/emails/emailVerification.ejs', {
-      user: user,
-      url: process.env.CLIENT_URL
-    });
+    const value = validate(registerSchema, req.body);
+    const userExists = await User.findOne({ email: value.email });
     
-    await sendMail(user.email, html);
+    if(!userExists) {
+      const user = await User.create(value);
+      const verificationToken = jwt.sign({id: user._id}, process.env.VERIFY_TOKEN_SECRET, {
+        expiresIn: '1d'
+      });
+      const html = await ejs.renderFile('./views/emails/emailVerification.ejs', {
+        user: user,
+        token: verificationToken,
+        url: process.env.CLIENT_URL
+      });
+
+      await sendMail(user.email, html);
+    }
 
     return res.status(200).json({
       message: 'Please check your email to verify your account'
