e142f8024c1f45b271b49fdbbe8d311b37c3d00f
novadityap <novaaditya23@gmail.com>
2024-06-09 20:37:51 +0700

    feat: Add middleware for body parsing

--- server/index.js
+++ server/index.js
@@ -10,6 +10,8 @@ const app = express();
 
 dbConnect();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(httpLoggerMiddleware);
 app.use('/api/auth', authRoute);
 app.use(errorMiddleware);
