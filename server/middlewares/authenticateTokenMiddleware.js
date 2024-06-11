92a8b16977acbcec86fcfdd6af0375759043d632
novadityap <novaaditya23@gmail.com>
2024-06-11 16:51:54 +0700

    fix: Check access token into blacklist model

--- server/middlewares/authenticateTokenMiddleware.js
+++ server/middlewares/authenticateTokenMiddleware.js
@@ -1,10 +1,14 @@
 import jwt from 'jsonwebtoken';
 import ResponseError from '../error/responseError.js';
+import Blacklist from '../models/blacklistModel.js';
 
-const authenticateTokenMiddleware = (req, res, next) => {
+const authenticateTokenMiddleware = async (req, res, next) => {
   try {
     const accessToken = req.cookies.accessToken;
     if(!accessToken) throw new ResponseError(401, 'Unauthorized');
+
+    const storedAccessToken = await Blacklist.findOne({token: accessToken});
+    if(storedAccessToken) throw new ResponseError(401, 'Unauthorized');
 
     const decoded = jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET);
     req.user = decoded;
